6160455b8749c0bf8e402d54f2350eb44884c728
EC2 Default User <ec2-user@ip-172-31-11-104.ap-northeast-1.compute.internal>
2020-08-09 10:45:11 +0000

    add reconnect process

--- game/room.go
+++ game/room.go
@@ -23,7 +23,7 @@ func NewRoom(roomId int) *Room {
     room := new(Room)
     room.RoomId = roomId
     room.Channel = make(chan Notification)
-    room.Observers = Observer{ UserNames: make([]string, 4, 4), UserRates: make([]int, 4, 4), Senders: make([]Sender, 0, 4), Subject: room.Channel, Status: Wait }
+    room.Observers = Observer{ UserNames: make([]string, 4, 4), UserRates: make([]int, 4, 4), Senders: make([]Sender, 0, 4), Subject: room.Channel, Status: Wait ,RoomId: room.RoomId}
     go func() { room.Observers.WaitNotice() }()
     return room
 }
@@ -43,4 +43,29 @@ func (room *Room)UserJoin(sequence int, connection net.Conn,userName string,rate
         //For debugging
         fmt.Println(room.Observers.Game)
     }
+}
+
+func (room *Room)Reconnect(connection net.Conn,userId int){
+    id:=-1
+    for i:=0;i<4;i++{
+        if room.Observers.Senders[i].Id==userId{
+            id=i
+        }
+    }
+    if id==-1{
+        connection.Close()
+        return
+    }
+    //make new receiver
+    var receiver Receiver = Receiver{ Id: id, Connection: connection, Observer: room.Channel }
+    go receiver.WaitMessage()
+    //sender reconfigure
+    room.Observers.Senders[id].Connection=connection
+    room.Observers.Senders[id].DummyFlag=false
+    messages:=room.Observers.Game.CreateInitMessage(room.Observers.UserNames,room.Observers.UserRates)
+    messages[id]["RoomID"]=room.Observers.RoomId
+    messages[id]["UserID"]=userId
+    room.Observers.Senders[id].SendMessage(messages[id])
+    messages=room.Observers.Game.CreateUpdateMessage()
+    room.Observers.Senders[id].SendMessage(messages[id])
 }
